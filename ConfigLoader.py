4981dfd6f52bdf56af90ef4de9561c20848320d5
ramkaeh <99654918+ramkaeh@users.noreply.github.com>
2024-07-05 12:58:47 +0200

    Update ConfigLoader.py

--- ConfigLoader.py
+++ ConfigLoader.py
@@ -3,3 +3,13 @@ import yaml
 class ConfigLoader:
   def __init__(self,config_file):
     self.config_file = config_file
+    self.config = slef.load_config()
+
+  def load_config(self):
+    with open(self.config_file,'r')
+    as file:
+      config = yaml.safe_load(file)
+
+    return config
+
+  def get_database_config
