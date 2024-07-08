57d72ef08292bc7f147fa471edddbca4afd184fc
ramkaeh <99654918+ramkaeh@users.noreply.github.com>
2024-07-08 15:57:59 +0200

    db shit

--- Database.py
+++ Database.py
@@ -1,1 +1,13 @@
+import sqlite3
+from sqlite3 import Error
 
+class Database:
+  _instance = None
+
+  def __new__(cls, db_file):
+    if cls._instance is None:
+      cls._instance = super(Database, cls).__new__(cls)
+
+  cls._instance._initialize(self, db_file)
+    return cls._instance
+return cls._instance
